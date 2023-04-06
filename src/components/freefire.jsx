0109b2e8af9aae526666b2f9a940709f79f048ce
TheCipherBit <Digitalfox1155@gmail.com>
2023-04-06 17:55:30 +0545

    ecommerce

--- src/components/freefire.jsx
+++ src/components/freefire.jsx
@@ -1,5 +1,25 @@
-export function Freefire(){
-    return(<>
-    this is freefire section
-    </>);
+import React from 'react';
+import "../components/freefire.css";
+import freefire from "../Images/freefire.jpg";
+
+const cardData = [
+  { id: 1, className: 'freefire-card-1', imageUrl: freefire },
+  { id: 2, className: 'freefire-card-2', imageUrl: freefire },
+  { id: 3, className: 'freefire-card-3', imageUrl: freefire },
+  { id: 4, className: 'freefire-card-4', imageUrl: freefire },
+];
+
+export function Freefire() {
+  return (
+    <>
+      <div className="card-container">
+        {cardData.map((card) => (
+          <div key={card.id} className={card.className}>
+            <img src={card.imageUrl} alt="Card" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
+
